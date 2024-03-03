a734edea38c4cc7299cd815045239b67e6364626
RIZKI DESU <s4y4pu1an6@gmail.com>
2024-03-03 23:48:44 +0700

    update

---
+++ src/app/components/AboutSection.jsx
@@ -0,0 +1,99 @@
+"use client";
+import React, { useTransition, useState } from "react";
+import Image from "next/image";
+import TabButton from "./TabButton";
+
+const TAB_DATA = [
+  {
+    title: "Skills",
+    id: "skills",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>IoT Development</li>
+        <li>Node.js</li>
+        <li>Laravel</li>
+        <li>MySQL</li>
+        <li>Python</li>
+        <li>JavaScript</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Education",
+    id: "education",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>MIPA - SMAN 1 BANTUR</li>
+        <li>Teknik Informatika - Universitas PGRI Kanjuruhan Malang</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Certifications",
+    id: "certifications",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Dicoding Indonesia Basic Machine Learning</li>
+        <li>Dicoding Indonesia Basic Dart Programming</li>
+        <li>Cisco Basic Data Analytic</li>
+        <li>Google Professional IT Support</li>
+      </ul>
+    ),
+  },
+];
+
+const AboutSection = () => {
+  const [tab, setTab] = useState("skills");
+  const [isPending, startTransition] = useTransition();
+
+  const handleTabChange = (id) => {
+    startTransition(() => {
+      setTab(id);
+    });
+  };
+
+  return (
+    <section className="text-white" id="about">
+      <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
+        <Image src="/images/about-image.png" width={500} height={500} />
+        <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
+          <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
+          <p className="text-base lg:text-lg">
+          Saya seorang pengembang fullstack prangkat IoT dengan hasrat untuk berkreasi di IoT development yang optimal. 
+          Saya mempunyai penganalam ndalam membuat projek IoT, node js, laravel, mysql, python, git. 
+          Saya seorang yang di tuntut belajar cepat karena pada bidang IoT memukinkan belajar lebih dari satu bahasa pemograman. 
+          Saya senang berkerjasama dengan tim, saya sangat suka terhadap perkembangan IT yang semakin pesat untuk sekarang.
+          </p>
+          <div className="flex flex-row justify-start mt-8">
+            <TabButton
+              selectTab={() => handleTabChange("skills")}
+              active={tab === "skills"}
+            >
+              {" "}
+              Skills{" "}
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("education")}
+              active={tab === "education"}
+            >
+              {" "}
+              Education{" "}
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("certifications")}
+              active={tab === "certifications"}
+            >
+              {" "}
+              Certifications{" "}
+            </TabButton>
+          </div>
+          <div className="mt-8">
+            {TAB_DATA.find((t) => t.id === tab).content}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default AboutSection;
